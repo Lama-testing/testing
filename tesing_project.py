5ba23962bfa189b8bdc20649da8e8190667a3813
jerusalem <jerusalem@DESKTOP-DAIF0TH>
2025-06-02 19:03:49 +0300

    Nada's Commit

--- tesing_project.py
+++ tesing_project.py
@@ -1,6 +1,7 @@
 import sys
 from time import sleep
 import pytest
+import uc
 import undetected_chromedriver as uc
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
@@ -21,7 +22,7 @@ def driver():
 
     driver = uc.Chrome(options=options)
     driver.maximize_window()
-    driver.get('https://www.skyscanner.co.il')
+    driver.get('https://www.skyscanner.co.il/')
     sleep(3)
     yield driver
     driver.quit()
@@ -133,3 +134,43 @@ def test_1(driver):
     results = driver.find_elements(By.XPATH,
                                    "//div[@class='HotelCardsListChunk_HotelCardsListChunk__card__O2gi3 HotelCardsListChunk_HotelCardsListChunk__card--newLayout__DTkR9 HotelCardsListChunk_Animation__zkf0j']")
     assert len(results) > 0, "No hotel results were found."
+    # //////////////////////////////////////////////////////////////////////////////
+# Hotel Search functionality with Invalid destination or hotel name
+def test_2(driver):
+    sleep(15)
+    Hotel_tab = driver.find_element(By.XPATH, "//span[text()='Hotels']")
+    Hotel_tab.click()
+    destination = driver.find_element(By.ID, 'destination-autosuggest')
+    destination.send_keys('invaliddestination')
+    sleep(3)
+    Check_in = driver.find_element(By.XPATH, "//input[@id='checkin']")
+    Check_in.click()
+    sleep(3)
+    checkin = driver.find_element(By.XPATH, "//span[text() = '20']")
+    checkin.click()
+    sleep(3)
+    Check_out = driver.find_element(By.XPATH, "//input[@id ='checkout']")
+    Check_out.click()
+    sleep(3)
+    checkout = driver.find_element(By.XPATH, "//span[text() = '26']")
+    checkout.click()
+    sleep(3)
+    Room_Adult= driver.find_element(By.XPATH,"//input[@id='guests-rooms']")
+    Room_Adult.click()
+    sleep(3)
+    Adults = driver.find_element(By.XPATH,"//input[@id='adults']")
+    Adults.clear()
+    Adults.send_keys('4')
+    sleep(3)
+    Rooms= driver.find_element(By.XPATH,"//input[@id='rooms']")
+    Rooms.clear()
+    Rooms.send_keys('2')
+    sleep(3)
+    BTN_Done = driver.find_element(By.XPATH,"//span[text()='Done']")
+    BTN_Done.click()
+    sleep(2)
+    btn_search = driver.find_element(By.CLASS_NAME,'BpkButton_bpk-button__ZGRmN.BpkButton_bpk-button--large__ZmE2M.bpk-button--submit.ExpandableLayout_ExpandableLayout__cta__OWY1Z')
+    btn_search.click()
+    sleep(5)
+    error_message= driver.find_element(By.XPATH,"//span[@class='BpkText_bpk-text__ZjI3M BpkText_bpk-text--body-default__ODg2M SearchControls__destinationErr--message']")
+    assert error_message.is_displayed(),'Error message not shown for invalid destination'
